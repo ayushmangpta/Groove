01f358fcb10487826033b2c231873ab6478c24f5
ayushmangpta <ayushmangpta@gmail.com>
2024-01-29 22:11:38 +0530

    added fragments

--- app/src/main/java/com/example/groove/MainActivity.java
+++ app/src/main/java/com/example/groove/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.groove;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottombar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottombar = findViewById(R.id.bottombar);
+        bottombar.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.profile){
+                    FragmentManager fm = getSupportFragmentManager();
+                    FragmentTransaction ft = fm.beginTransaction();
+                    ft.add(R.id.container, new MyProfile());
+                    ft.commit();
+
+                } else if (id== R.id.home) {FragmentManager fm = getSupportFragmentManager();
+                    FragmentTransaction ft = fm.beginTransaction();
+                    ft.add(R.id.container, new home());
+                    ft.commit();
+                    
+                }
+                else{
+                    FragmentManager fm = getSupportFragmentManager();
+                    FragmentTransaction ft = fm.beginTransaction();
+                    ft.add(R.id.container, new Add());
+                    ft.commit();
+
+                }
+            }
+        });
+        System.out.println();
     }
 }
